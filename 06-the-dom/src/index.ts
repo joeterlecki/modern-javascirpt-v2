b83a83a0f370d5a4be7fbfb4f540e88cd138e871
Joe Terlecki <joe.p.terlecki@gmail.com>
2023-02-16 23:34:43 -0500

    inseting dom

--- 06-the-dom/src/index.ts
+++ 06-the-dom/src/index.ts
@@ -71,5 +71,157 @@
 /**
  * MULTIPLE ELEMENTS
  */
-const listItems: NodeListOf<HTMLElement> = document.querySelectorAll(".item")
-listItems.forEach((item) => console.log(item.innerText))
+// const listItems: NodeListOf<HTMLElement> = document.querySelectorAll(".item")
+// // listItems.forEach((item, index) => {
+// //   item.style.color = "red"
+// //   if (index === 1) {
+// //     item.remove()
+// //   }
+
+// //   if (index === 0) {
+// //     item.innerHTML = `Oranges
+// //     <button class="remove-item btn-link text-red">
+// //     <i class="fa-solid fa-xmark"></i>
+// //     </button>`
+// //   }
+// // })
+
+// const listItems2 = document.getElementsByClassName("item")
+// console.log(listItems2[2].textContent)
+
+// const listItemsArray = Array.from(listItems2)
+// console.log(listItemsArray[1].textContent)
+
+/**
+ * TRAVERSING ELEMENTS
+ */
+//get child elements
+
+// let output
+// const parent = document.querySelector(".parent")
+
+// output = parent.children
+// output = parent.children[1].innerText
+// output = parent.children[1].className
+// output = parent.children[1].nodeName
+
+// parent.children[1].textContent = "CHILD TWO"
+// parent.children[1].style.color = "red"
+// parent.firstElementChild.textContent = "CHILD ONE"
+// parent.lastElementChild.textContent = "CHILD THREE"
+
+// // get parent element from the child
+
+// const child = document.querySelector(".child")
+
+// output = child.parentElement
+// child.parentElement.style.border = "1px solid #ccc"
+// child.parentElement.style.padding = "10px"
+
+// // sibline elements
+
+// const secondItem = document.querySelector(".child:nth-child(2)")
+// output = secondItem
+
+// output = secondItem.nextElementSibling
+// secondItem.nextElementSibling.style.color = "green"
+// secondItem.previousElementSibling.style.color = "orange"
+
+// console.log(output)
+
+/**
+ * DOM NODES
+ */
+
+/**
+ * CREATING ELEMENTS
+ */
+
+// const div: HTMLDivElement = document.createElement("div")
+// div.className = "created-div"
+// div.id = "created-dev-id"
+// div.setAttribute("title", "My Elements")
+
+// const text: Text = document.createTextNode("HELLO WORLD")
+// div.appendChild(text)
+
+// document.body.appendChild(div)
+
+// document.querySelector("ul")?.appendChild(div)
+
+// console.log(div)
+
+// quick and dirty
+// function createListItem(item: string): void {
+//   const li = document.createElement("li")
+//   li.innerHTML = `${item}
+//   <button class="remove-item btn-link text-red">
+//     <i class="fa-solid fa-xmark"></i>
+//   </button>`
+
+//   document.querySelector(".items")?.appendChild(li)
+// }
+
+// function createNewItem(item: string): void {
+//   const li: HTMLLIElement = document.createElement("li")
+//   li.appendChild(document.createTextNode(item))
+
+//   const button = createButton("remove-item btn-link text-red")
+
+//   li.appendChild(button)
+
+//   document.querySelector(".items")?.appendChild(li)
+// }
+
+// function createButton(classes: string): HTMLButtonElement {
+//   const button: HTMLButtonElement = document.createElement("button")
+//   button.className = classes
+//   const icon = createIcon("fa-solid fa-xmark")
+//   button.appendChild(icon)
+
+//   return button
+// }
+
+// function createIcon(classes: string): HTMLElement {
+//   const icon: HTMLElement = document.createElement("i")
+//   icon.className = classes
+//   return icon
+// }
+
+// createNewItem("Cheese")
+// createNewItem("Pizza")
+
+/**
+ * Insert elements text and html
+ */
+
+/**
+ * <!-- beforebegin -->
+ * <p>
+ *   <!-- afterbegin -->
+ *   foo
+ *   <!-- beforeend -->
+ * </p>
+ * <!-- afterend -->
+ */
+
+function insertElement(): void {
+  const filter = document.querySelector(".filter") as HTMLElement
+  const h1: HTMLHeadingElement = document.createElement("h1")
+  h1.textContent = "insertAdjacentElement"
+  filter?.insertAdjacentElement("beforebegin", h1)
+}
+
+function insertText(): void {
+  const item = document.querySelector("li:first-child") as HTMLElement
+  item.insertAdjacentText("beforebegin", "insertAdjacentText")
+}
+
+function insertHTML(): void {
+  const clearBTN = document.querySelector("#clear") as HTMLElement
+  clearBTN.insertAdjacentHTML("afterend", "<h2>insertAdjacentHTML</h2>")
+}
+
+insertElement()
+insertText()
+insertHTML()
